a4dd798e276887aa2e1c7ae5ada56909fc473617
unknown <eric.wan.97@gmail.com>
2018-04-15 21:14:50 -0400

    week2hw2

--- HW2/ezw23_HW2.m
+++ HW2/ezw23_HW2.m
@@ -40,53 +40,115 @@ figure;
 hold on;
 plot(xt, x);
 plot(yt, y);
-
-
+title("Question 12")
+xlabel("Values");
+ylabel("Counts");
+legend("Target Absent", "Target Present");
 
 %% Question 13
 data = xlsread("HW2_data_shankar_Spring.xlsx", "hw5data_", "CE:CE");
-data1 = data(1:40,:); % target is present
+data1 = data(1:40); % target is not present
 data1 = sort(data1);
-data2 = data(41:70,:); % target is not present
+data2 = data(41:70); % target is present
 data2 = sort(data2);
-[p, pt] = ksdensity(data1);
-[np, npt]= ksdensity(data2);
-%{
+thr = (median(data1) + median(data2))/2;
+
 figure;
 hold on;
-hist(p);
-hist(np);
-%}
-figure;
-hold on;
-plot(pt, p);
-plot(npt, np);
-area(pt(pt>4.1 & pt<pt(end)), p(pt>4.1 & pt<pt(end)));
+grid on;
+histogram(data1, 6);
+histogram(data2, 6);
+line(thr, 10, 'Color', 'red', 'LineWidth', 3);
+title("Question 13: WAN");
+xlabel("Values");
+ylabel("Counts");
+legend("Target Absent", "Target Present");
 
-
-
-thr = 4.7; % threshold
-Nx = numel(data2); % # of elements in X (No target)
-Ny = numel(data1); % # of elements in Y (W/ target)
+Nx = numel(data1); % # of elements in d2 (No target)
+Ny = numel(data2); % # of elements in d1 (W/ target)
 N = Nx + Ny; % # of total elements
 
-Nf = X > thr; % samples X > threshold
-count = sum(X > thr); % # of samples X > threshold
-pTa = Nx/N; % a priori probability for X (no target was present)
-pF = count/Nx; % probability of false alarm (X > thr / count X)
+Nf = sum(data1 > thr); % samples d2 > threshold
+pTa = Nx/N; % a priori probability for d2 (no target was present)
+pF = Nf/Nx; % probability of false alarm (d2 > thr / count d2)
 
-Nc = Y > thr; % samples Y- > threshold
-count = sum(Y > thr); % # of samples Y > threshold
-pTp = Ny/N; % a priori probabilty for Y (target was present)
-pD = count/Ny; % probability of detection (Y > thr / count Y)
-pM = 1 - pD;
+Nc = sum(data2 > thr); % samples d1 > threshold
+pTp = Ny/N; % a priori probabilty for d1 (target was present)
+pD = Nc/Ny; % probability of detection (d1 > thr / count d1)
+pM = 1 - pD; % probabilty of miss
+
+pSens = Nf/Ny; % # of correct positive detections / # of positive cases
+pSpec = Nc/Nx; % # of correct negative detections / # of negative cases
+PPV = Nc / (Nc + Nf); % # of correct positive detections / # of positive detections
+pACC = (Nc + (Nx - Nf)) / N; % # of correct detections / # of cases
 
 pE = pM * pTp + pF * pTa; % prob error = prob miss * a priori prob present + prob fail * apriori prob not present
 % a priori prob, false alarm, miss, sensitivity, specificity, positive predictive value, accuracy
-sprintf("a priori probabilty for X (target was not present) of threshold (%f): %f", thr, pTa)
-sprintf("a priori probabilty for Y (target was present) of threshold (%f): %f", thr, pTp)
-sprintf("probabilty for a false alarm (target was not present but detects as such) of threshold (%f): %f", thr, pF)
-sprintf("probabilty for a miss (target was present but not detects) of threshold (%f): %f", thr, pM)
+sprintf("a priori probabilty for X of threshold (%f): %f", thr, pTa)
+sprintf("a priori probabilty for Y of threshold (%f): %f", thr, pTp)
+sprintf("probabilty for a false alarm of threshold (%f): %f", thr, pF)
+sprintf("probabilty for a miss of threshold (%f): %f", thr, pM)
+sprintf("sensitivity of threshold (%f): %f", thr, pSens)
+sprintf("specificity of threshold (%f): %f", thr, pSpec)
+sprintf("positive predictive value of threshold (%f): %f", thr, PPV)
+sprintf("accuracy of threshold (%f): %f", thr, pACC)
 
+fprintf('------Confusion Matrix for Histogram w/ Threshold %f------\n', thr);
+fprintf('%-15s\t %-15s\t %-15s\t %-15s\n', 'Data', 'Target', 'Target', 'Total')
+fprintf('%-15s\t %-15s\t %-15s\t %-15s\n', 'Collected', 'Detected', 'Not Detected', 'Count')
+fprintf('%-15s\t %-15d\t %-15d\t %-15d\n', 'Target Absent',Nf, Nx - Nf, Nx)
+fprintf('%-15s\t %-15d\t %-15d\t %-15d\n', 'Target Present', Nc, Ny - Nc, Ny)
+fprintf('%-15s\t %-15d\t %-15d\t %-15d\n', 'Total Count', Nf + Nc, Ny - Nf + Ny - Nc, N)
 
-% ^^sensitivity, specificity, positive predictive value, accuracy
+pt = 0:0.0001:10;
+npt = 0:0.0001:25;
+p = ksdensity(data1, pt);
+np = ksdensity(data2, npt);
+thr = sum((pt((abs(p - np(1:numel(p))) < 0.00001)) + npt((abs(p - np(1:numel(p))) < 0.00001)))/2)/2;
+figure;
+hold on;
+plot(pt, p); % absent
+plot(npt, np); % present
+area(pt(pt>thr & pt<pt(end)), p(pt>thr & pt<pt(end))); % miss
+area(npt(npt<thr & npt>npt(1)), np(npt<thr & npt>npt(1))); % false alarm
+title("Question 13: WAN");
+xlabel("Values");
+ylabel("Est. Density");
+legend("Target Absent", "Target Present", "False Alarm", "Miss");
+
+Nx = numel(data1); % # of elements in d2 (No target)
+Ny = numel(data2); % # of elements in d1 (W/ target)
+N = Nx + Ny; % # of total elements
+
+Nf = sum(data1 > thr); % samples d2 > threshold
+pTa = Nx/N; % a priori probability for d2 (no target was present)
+pF = Nf/Nx; % probability of false alarm (d2 > thr / count d2)
+
+Nc = sum(data2 > thr); % samples d1 > threshold
+pTp = Ny/N; % a priori probabilty for d1 (target was present)
+pD = Nc/Ny; % probability of detection (d1 > thr / count d1)
+pM = 1 - pD; % probabilty of miss
+
+pSens = Nf/Ny; % # of correct positive detections / # of positive cases
+pSpec = Nc/Nx; % # of correct negative detections / # of negative cases
+PPV = Nc / (Nc + Nf); % # of correct positive detections / # of positive detections
+pACC = (Nc + (Nx - Nf)) / N; % # of correct detections / # of cases
+
+pE = pM * pTp + pF * pTa; % prob error = prob miss * a priori prob present + prob fail * apriori prob not present
+% a priori prob, false alarm, miss, sensitivity, specificity, positive predictive value, accuracy
+sprintf("a priori probabilty for X of threshold (%f): %f", thr, pTa)
+sprintf("a priori probabilty for Y of threshold (%f): %f", thr, pTp)
+sprintf("probabilty for a false alarm of threshold (%f): %f", thr, pF)
+sprintf("probabilty for a miss of threshold (%f): %f", thr, pM)
+sprintf("sensitivity of threshold (%f): %f", thr, pSens)
+sprintf("specificity of threshold (%f): %f", thr, pSpec)
+sprintf("positive predictive value of threshold (%f): %f", thr, PPV)
+sprintf("accuracy of threshold (%f): %f", thr, pACC)
+
+fprintf('------Confusion Matrix for kDensity w/ Threshold %f------\n', thr);
+fprintf('%-15s\t %-15s\t %-15s\t %-15s\n', 'Data', 'Target', 'Target', 'Total')
+fprintf('%-15s\t %-15s\t %-15s\t %-15s\n', 'Collected', 'Detected', 'Not Detected', 'Count')
+fprintf('%-15s\t %-15d\t %-15d\t %-15d\n', 'Target Absent',Nf, Nx - Nf, Nx)
+fprintf('%-15s\t %-15d\t %-15d\t %-15d\n', 'Target Present', Nc, Ny - Nc, Ny)
+fprintf('%-15s\t %-15d\t %-15d\t %-15d\n', 'Total Count', Nf + Nc, Ny - Nf + Ny - Nc, N)
+
